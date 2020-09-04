5db0536536638fb43bed912748da69b070888508
LEE EUNJIN <lejerin1112@gmail.com>
2020-09-04 23:57:30 +0900

    room_kotlin

--- room_kotlin/app/src/main/java/lej/modern/room_kotlin/MainActivity.kt
+++ room_kotlin/app/src/main/java/lej/modern/room_kotlin/MainActivity.kt
@@ -2,29 +2,34 @@ package lej.modern.room_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.Observer
+import androidx.activity.viewModels
+import androidx.lifecycle.*
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val db = Room.databaseBuilder(
-            applicationContext,
-            AppDatabase::class.java, "todo-db"
-        ).allowMainThreadQueries()
-            .build()
 
-        db.todoDao().getAll().observe(this, Observer { todos ->
-            result_text.setText(todos.toString())
+        val viewModel: MainViewModel by viewModels()
 
+        viewModel.getAll().observe(this, Observer { todos ->
+            result_textview.text = todos.toString()
         })
 
         add_btn.setOnClickListener {
-            db.todoDao().insert(Todo(todo_edit.text.toString()))
+            lifecycleScope.launch(Dispatchers.IO) {
+                viewModel.insert(Todo(todo_edit.text.toString()))
+            }
+
         }
+
 
     }
 }
--- room_kotlin/app/src/main/java/lej/modern/room_kotlin/Todo.kt
+++ room_kotlin/app/src/main/java/lej/modern/room_kotlin/Todo.kt
@@ -4,6 +4,8 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class Todo(var title: String ){
-    @PrimaryKey(autoGenerate = true) val id: Int = 0
+data class Todo(
+            var title: String
+){
+    @PrimaryKey(autoGenerate = true) var id: Int = 0
 }
